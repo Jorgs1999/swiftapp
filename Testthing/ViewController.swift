1e9e7246b3fe087df4962db655af2bf1772cf375
Jacob Jorgensen <17Jorgensenj@badinhs.org>
2016-09-21 08:10:32 -0400

    calc first commit

--- Testthing/ViewController.swift
+++ Testthing/ViewController.swift
@@ -2,13 +2,42 @@
 //  ViewController.swift
 //  Testthing
 //
-//  Created by BHSRam4 on 9/9/16.
+//  Created by BHSRam4 on 9/13/16.
 //  Copyright © 2016 BHSRam4. All rights reserved.
 //
 
 import UIKit
 
+
+
+
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    
+    
+    var tapCount=0
+    
+    @IBAction func button1tapped(_ sender: AnyObject) {
+        
+      label.text="Now Cooler!"
+        print("button tapped")
+        
+        tapCount=tapCount+1
+        
+        if tapCount >= 20 {
+        label.text="You have tapped 20 or more times"
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
